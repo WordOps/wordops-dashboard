cad67a4cd83ec75dd39f3629d04c8c7f44fc8510
VirtuBox <thomas@virtubox.net>
2019-08-15 11:28:41 +0200

    keep fontawesome 5.8.1

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ $root = $_SERVER['HTTP_HOST'];
   </script>
   <script type="text/javascript"
     src="https://<?php echo $root; ?>/netdata/dashboard.js"></script>
-  <script defer src="https://use.fontawesome.com/releases/v5.10.1/js/all.js"
+  <script defer src="https://use.fontawesome.com/releases/v5.8.1/js/all.js"
     integrity="sha384-g5uSoOSBd7KkhAMlnQILrecXvzst9TdC09/VM+pjDTCM+1il8RHz5fKANTFFb+gQ" crossorigin="anonymous">
   </script>
   <style>
